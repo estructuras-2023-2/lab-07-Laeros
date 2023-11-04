da9731b6e061f81fa6ab6f533d3e5b9cc6e8d7ba
Laeros <104449811+Laeros@users.noreply.github.com>
2023-11-03 20:30:37 -0500

    For para el proceso de una ronda del torneo

--- student_file.h
+++ student_file.h
@@ -4,6 +4,9 @@
 using namespace std;
 
 vector<int> Torneo(vector<int> playerSkills, int numPlayers, int numRounds) {
+    queue<int> playerQueue;
+    for (size_t i = 2; i < playerSkills.size(); ++i) {
+        playerQueue.push(playerSkills[i]);
     }
 
     int wins = 0;
@@ -11,6 +14,21 @@ vector<int> Torneo(vector<int> playerSkills, int numPlayers, int numRounds) {
     int challenger = playerSkills[1];
 
     vector<int> result(2);
-    for (int round = 1; round <= numRounds; ++round) { 
+    for (int round = 1; round <= numRounds; ++round) {
+        if (currentPlayer > challenger) {
+            wins++;
+            result = {challenger, currentPlayer};
+            playerQueue.push(challenger);
+        } else {
+            wins = 1;
+            result = {currentPlayer, challenger};
+            playerQueue.push(currentPlayer);
+            currentPlayer = challenger;
         }
+
+        challenger = playerQueue.front();
+        playerQueue.pop();
+    }
+
+    return result;
 }
