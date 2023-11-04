68fa2625103ad29eb1ac0a437a662176858dd70c
Laeros <104449811+Laeros@users.noreply.github.com>
2023-11-03 20:19:11 -0500

    Creando el archivo student_file.h

---
+++ student_file.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> Torneo(vector<int> playerSkills, int numPlayers, int numRounds) {
+    }
+
+    int wins = 0;
+    int currentPlayer = playerSkills[0];
+    int challenger = playerSkills[1];
+
+    vector<int> result(2);
+    for (int round = 1; round <= numRounds; ++round) { 
+        }
+}
